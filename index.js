f275529e6b538772288a8c0953eff8f8ad2b7ebb
rnrygus061113 <68359748+rnrygus061113@users.noreply.github.com>
2020-07-18 14:19:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -275,7 +275,7 @@ function changeCommandStringLength(str, limitLen = 8) {
 }
 
   let msg = await message.channel.send(str);
-async function AutoMsgDelete(message, str, delay = 9999999999999999999999999999) {
+async function AutoMsgDelete(message, str, delay = 900000) {
   
   setTimeout(() => {
     msg.delete();
